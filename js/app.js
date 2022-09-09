47044da6d2e579ed99cc0dca6259a078c2a007e4
Ledio Dylgjeri <lediodylgjeri@gamil.com>
2022-09-09 19:02:26 -0400

    set varibales for cached elements

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-console.log('hello');
+const board = document.querySelector('#game-board')
+const message = document.getElementById('#meesage')
+const rstButton = document.getElementById('#rst-btn')
+
+
