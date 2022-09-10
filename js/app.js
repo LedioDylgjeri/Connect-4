5693aa2418b115108241d4b7af42cbd15df7d607
Ledio Dylgjeri <lediodylgjeri@gamil.com>
2022-09-10 12:55:33 -0400

    add event listener to board

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
-const board = document.querySelector('#game-board')
+/* ------------------------------- Constants ------------------------------------ */
+
+
+/* --------------------------- Variables (state) -------------------------------- */
 const message = document.getElementById('#meesage')
+const boardEl = document.querySelector('section')
+console.log(boardEl);
 const rstButton = document.getElementById('#rst-btn')
+
+
+/* ----------------------- Cached Element Rererences ---------------------------- */
+boardEl.addEventListener('click', function(evt) {
+  console.log(evt.target);
+})
+
+
+/* ------------------------------ Functions ------------------------------------- */
 
 
